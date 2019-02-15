1881108e6268bbcd368140a80f8f7cccb76f33a5
Ryan Furrer <rfurrer@ualberta.ca>
2019-02-15 12:50:13 -0700

    Some reflexion

--- lib/cmd.rb
+++ lib/cmd.rb
@@ -13,7 +13,9 @@ class Cmd
     init_reader
   end
 
-  def init_reader; end
+  def init_reader
+    set_autocomplete
+  end
 
   def loop_setup
     puts @welcome
@@ -28,14 +30,33 @@ class Cmd
     while (input = Readline.readline(@prompt, add_hist_true))
       next if input == ''
 
-      process_cmd
+      input = input.split(' ')
+
+      if !(new_cmd = get_cmd input[0]).nil?
+        process_cmd new_cmd, (input.slice 1, -1)
+      else
+        handle_unknown_cmd input[0]
+      end
+
     end
     loop_finish
   end
 
-  def process_cmd; end
+  def get_cmd(command)
+    self.class.instance_methods.select { |m| m.to_s.eql? 'do_' + command }[0]
+  end
 
-  def handle_unknown_cmd; end
+  # Create child/worker process
+  # Parent : wait for worker (child) to finish
+  # Child: change job
+  # Parent: report results
+  def process_cmd(command, *args)
+    send command, args
+  end
+
+  def handle_unknown_cmd(input)
+    puts 'Invalid command: ' + input
+  end
 
   def prev_hist
     return unless @history_index.positive?
@@ -51,5 +72,10 @@ class Cmd
     @history_index += 1
   end
 
-  def help_help; end
+  def set_autocomplete
+    comp = proc { |s| Dir.entries('.').grep(/^#{Regexp.escape(s)}/) }
+
+    Readline.completion_append_character = "\t"
+    Readline.completion_proc = comp
+  end
 end
--- lib/shell_commands.rb
+++ lib/shell_commands.rb
@@ -1,8 +1,27 @@
 # Basic Commands for a Ruby Shell
 module ShellCommands
+
+  def help_help; end
+
   def do_help; end
 
   def do_cd; end
 
   def do_exit; end
+
+  def do_ls; end
+
+  def do_mv; end
+
+  def do_rm; end
+
+  def do_mkdir; end
+
+  def do_touch; end
+
+  def do_echo; end
+
+  def do_cat; end
+
+  def do_date; end
 end
