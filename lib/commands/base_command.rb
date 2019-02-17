a45a1009d757ec134b2757e0813ab1280cf75657
James Hryniw <jhryniw@gmail.com>
2019-02-17 01:00:53 -0700

    Better ls functionality (#8)
    * basic ls command
    
    * better ls functionality

--- lib/commands/base_command.rb
+++ lib/commands/base_command.rb
@@ -5,4 +5,8 @@ module BaseCommand
   def split_args(input)
     input.strip.split(' ')
   end
+
+  def term_columns
+    ENV['COLUMNS'].to_i
+  end
 end
--- lib/commands/ls_command.rb
+++ lib/commands/ls_command.rb
@@ -4,6 +4,8 @@ require_relative 'base_command'
 class LsCommand
   include BaseCommand
 
+  SEP = '  '
+
   def initialize(input)
     args = split_args input
     @opts = Slop.parse args do |o|
@@ -17,12 +19,51 @@ class LsCommand
   end
 
   def execute
-    files = Dir.entries(@opts.arguments[0])
+    begin
+      files = Dir.entries(@opts.arguments[0])
+    rescue SystemCallError
+      $stderr.print "Error: " + $!
+      return
+    end
 
     unless @opts.all?
       files.select! { |f| not f.start_with?('.') }
     end
 
-    puts files
+    old_dir = Dir.pwd
+    Dir.chdir(@opts.arguments[0])
+    print_files(files)
+    Dir.chdir(old_dir)
+  end
+
+  private
+
+  def color_by_type(f)
+    if File.directory?(f)
+      ColorText.blue(f)
+    elsif File.executable?(f)
+      ColorText.green(f)
+    else
+      f
+    end
+  end
+
+  def print_files(files)
+    cols = term_columns
+    buf = []
+    buf_len = 0
+
+    files.each do |f|
+      if buf_len + SEP.length + f.length > cols
+        puts buf.join(SEP)
+        buf.clear
+        buf_len = 0
+      end
+
+      buf.push(color_by_type(f))
+      buf_len += f.length + SEP.length
+    end
+
+    puts buf.join(SEP)
   end
 end
