919b08768e3859c723e2e69bcd14e5f4bb3c001a
Fraser <fbulbuc@ualberta.ca>
2019-02-19 17:04:28 -0700

    mv and touch

--- lib/commands/base_command.rb
+++ lib/commands/base_command.rb
@@ -2,6 +2,17 @@ class NotEnoughArgumentsError < StandardError
 end
 
 module BaseCommand
+
+  def parse_default_opts(input)
+    args = split_args input
+    opts = Slop.parse args do |o|
+      o.bool '-h', '--help'
+    end
+
+    opts.arguments.shift
+    opts
+  end
+
   def split_args(input)
     input.strip.split(' ')
   end
