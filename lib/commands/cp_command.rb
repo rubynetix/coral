63302be8da276b2cac459ea9a2fd4cb956aee587
Jordan Lane <jlane@ualberta.ca>
2019-02-20 12:31:05 -0800

    Jordan/cp (#34)
    * Cp Command
    
    * Cp Command Recursive and Verbose options

--- lib/commands/cp_command.rb
+++ lib/commands/cp_command.rb
@@ -13,8 +13,16 @@ class CpCommand
   EOS
 
   def initialize(input)
-    @opts = parse_default_opts(input)
+
+    args = split_args input
+    @opts = Slop.parse args do |o|
+      o.bool '-r', '--recursive', "Remove directories and their contents."
+      o.bool '-v', '--verbose', "Display verbose print messages"
+      o.bool '-h', '--help'
+    end
+
     @opts.options.banner = USAGE
+    @opts.arguments.shift
     @src, @dst = @opts.arguments
   end
 
@@ -24,6 +32,15 @@ class CpCommand
       return
     end
 
-    FileUtils.cp(@src, @dst)
+    begin
+      if @opts.recursive?
+        FileUtils.cp_r(@src, @dst, verbose: @opts.verbose?)
+      else
+        FileUtils.cp(@src, @dst, verbose: @opts.verbose?)
+      end
+    rescue SystemCallError => e
+      $stderr.print "#{e.message}\n"
+    end
+
   end
 end
