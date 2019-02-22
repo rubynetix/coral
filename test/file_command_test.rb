09339bf86bb6998d7473d4b38b85ce0e6246136d
Jordan-Lane <jlane@ualberta.ca>
2019-02-22 15:52:07 -0800

    Cp Command Tests

--- test/file_command_test.rb
+++ test/file_command_test.rb
@@ -3,6 +3,7 @@ require 'securerandom'
 require_relative '../lib/commands/ls_command'
 require_relative '../lib/commands/cd_command'
 require_relative '../lib/commands/cat_command'
+require_relative '../lib/commands/cp_command'
 require_relative '../lib/color_text'
 
 class FileCommandTest < Test::Unit::TestCase
@@ -161,6 +162,44 @@ class FileCommandTest < Test::Unit::TestCase
         unless valid_file(tokens[1])
           assert_false($stderr.string.empty?, "cat: Invalid file path should print to stderr")
         end
+      end
+    end
+  end
+
+  def test_cp
+    cp_exps = [
+        ['cp', nil],
+        ['cp .hidden_file', nil ],
+        ['cp invalid_file subdir/new_file', nil],
+        ['cp .hidden_file .hidden_file_copy', "#{@test_dir}/.hidden_file_copy" ],
+        ['cp RandomText.txt subdir/RandomTextSubdir.txt', "#{@test_dir}/subdir/RandomTextSubdir.txt" ]
+        #['cp subdir/hello.txt .', "#{@test_dir}/hello.txt"]
+    ]
+
+    cp_exps.each do |cmd, expected_file|
+      tokens = cmd.strip.split(' ')
+
+      # Preconditions
+      begin
+        assert_equal('cp', tokens[0])
+      end
+
+      $stdout.reopen
+      CpCommand.new(tokens).execute
+
+      # Postconditions
+      begin
+        unless expected_file.nil?
+          original_file = File.expand_path("#{@test_dir}/#{tokens[1]}")
+
+          assert_true(valid_file(expected_file), "cp: File not created properly")
+          assert_true(FileUtils.identical?(original_file, expected_file), "cp: Contents of the copied file is different from the original file")
+        end
+
+        unless valid_file(tokens[1])
+          assert_false($stderr.string.empty?, "cat: Invalid file path should print to stderr")
+        end
+
       end
     end
   end
