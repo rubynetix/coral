9492781053dfe098a8c6317d2f22dae1db758fa0
Fraser <fbulbuc@ualberta.ca>
2019-02-24 07:36:13 -0700

    Fix failing cp test

--- test/file_command_test.rb
+++ test/file_command_test.rb
@@ -254,7 +254,7 @@ class FileCommandTest < Test::Unit::TestCase
           assert_equal(Set.new(expected_files), Set.new(source_files), "cp: Recursive copy should copy all files")
         end
 
-        unless valid_file(tokens[1])
+        unless valid_file(tokens[2])
           assert_false($stderr.string.empty?, "cp: Invalid file path should print to stderr")
         end
       end
